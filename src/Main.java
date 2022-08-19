124aace93e558abe832c533f12f82b47fca0571d
Jaksa Jokunen <testinumero123@gmail.com>
2022-08-19 11:10:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 20;
+        int summa = 0;
+        if (a == b)
+        {
+            summa = (a*a)+(b*b);
+        }
+        else
+            summa = a+b;
+        System.out.println(summa);
+    }
+}
